eee48ec68f979918e0c0e9131868cfd9413a73e1
j-grosse <103175813+j-grosse@users.noreply.github.com>
2024-04-23 12:43:34 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur
+      reiciendis suscipit sint inventore minus eius deserunt repellendus
+      voluptates iste exercitationem ipsam doloremque dicta cupiditate nulla
+      consequuntur nesciunt, nam rem sit!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Products</h2>
+    <p>Product details for {{ id }}</p>
+
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos
+      cupiditate, eaque obcaecati, officia accusantium, vero possimus placeat
+      harum optio fugiat excepturi eos itaque. Quos dolorem dignissimos quasi
+      quam voluptates. Minima?
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
